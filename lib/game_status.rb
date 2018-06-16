9934a92671755fddc3544f40a8353ce07f326fda
Juan Tenezaca <tenj34@gmail.com>
2018-06-16 18:46:27 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -14,3 +14,14 @@ WIN_COMBINATIONS = [
   [0,4,8],
   [6,4,2]
 ]
+
+def won?(board)
+  WIN_COMBINATIONS.each_with_index do |element,index|
+    if position_taken?(board,element[0]) == true
+      if board[element[0]] == board[element[1]] && board[element[1]] == board[element[2]]
+        return WIN_COMBINATIONS[index]
+      end
+    end
+  end
+  return false
+end
