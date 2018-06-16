82052eb694e0664d4ca7b608a835a12e6d7bb1bd
Juan Tenezaca <tenj34@gmail.com>
2018-06-16 19:20:14 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -38,7 +38,9 @@ end
 
 #  returns true if the board has been won, is a draw, or is full
 def over?(board)
-  if draw?(board) == true  || full?(board) == true || won?(board) == true
-    return true
+  while won?(board)
+    if draw?(board) == true  || full?(board) == true
+      return true
+    end
   end
 end
